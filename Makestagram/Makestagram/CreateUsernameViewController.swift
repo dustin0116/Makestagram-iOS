925a87a648b7617d724900ceb97f42cafd8f149b
Dustin Hsiang <dustin0116@gmail.com>
2017-06-26 15:05:01 -0700

    Added Service Class.

---
+++ Makestagram/Makestagram/CreateUsernameViewController.swift
@@ -0,0 +1,58 @@
+//
+//  CreateUsernameViewController.swift
+//  Makestagram
+//
+//  Created by Dustin Hsiang on 6/26/17.
+//  Copyright © 2017 Dustin Hsiang. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+class CreateUsernameViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+
+            guard let firUser = Auth.auth().currentUser,
+                let username = usernameTextField.text,
+                !username.isEmpty else { return }
+            
+            UserService.create(firUser, username: username) { (user) in
+                guard let user = user else { return }
+                
+                print("Created new user: \(user.username)")
+            }
+        }    
+    
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
