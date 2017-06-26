8f0409d98698f3141f8cb278af19d8531b6f9b94
Dustin Hsiang <dustin0116@gmail.com>
2017-06-26 11:28:52 -0700

    Login/Register working and information going through Firebase.

--- Makestagram/Makestagram/Controllers/LoginViewController.swift
+++ Makestagram/Makestagram/Controllers/LoginViewController.swift
@@ -7,18 +7,40 @@
 //
 
 import UIKit
+import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
 
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var loginButton: UIButton!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func loginButtonTapped(_ sender: UIButton) {
-        print("login button tapped")
+        
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else { return }
+        
+        authUI.delegate = self
+        
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
+    }
+    
+}
+
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        
+        print("handle user signup / login")
     }
 }
