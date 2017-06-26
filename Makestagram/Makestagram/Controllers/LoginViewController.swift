a85cef53901bbc729b69b1ac07cad79a7267cd8e
Dustin Hsiang <dustin0116@gmail.com>
2017-06-26 11:15:33 -0700

    added IBAction of login button and is working when tapped.

--- Makestagram/Makestagram/Controllers/LoginViewController.swift
+++ Makestagram/Makestagram/Controllers/LoginViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,20 +18,7 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        print("login button tapped")
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
