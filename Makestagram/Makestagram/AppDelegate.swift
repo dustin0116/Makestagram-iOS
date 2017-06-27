9763e354e0f5f1318f0df80615e325f56d66f5e8
Dustin Hsiang <dustin0116@gmail.com>
2017-06-27 11:06:32 -0700

    added logic to AppDelegate so that user can be kept logged in.

--- Makestagram/Makestagram/AppDelegate.swift
+++ Makestagram/Makestagram/AppDelegate.swift
@@ -11,23 +11,16 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
--> Bool {
+        -> Bool {
             FirebaseApp.configure()
             
-            let storyboard = UIStoryboard(name: "Login", bundle: .main)
+            configureInitialRootViewController(for: window)
             
-            if let initialViewController = storyboard.instantiateInitialViewController() {
-                
-                window?.rootViewController = initialViewController
-                
-                window?.makeKeyAndVisible()
-            }
-
             return true
     }
     
@@ -54,5 +47,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+extension AppDelegate {
+    func configureInitialRootViewController(for window: UIWindow?) {
+        let defaults = UserDefaults.standard
+        let initialViewController: UIViewController
+        
+        if Auth.auth().currentUser != nil,
+            let userData = defaults.object(forKey: Constants.UserDefaults.currentUser) as? Data,
+            let user = NSKeyedUnarchiver.unarchiveObject(with: userData) as? User {
+            User.setCurrent(user)
+            
+            initialViewController = UIStoryboard.initialViewController(for: .main)
+        } else {
+            initialViewController = UIStoryboard.initialViewController(for: .login)
+        }
+        window?.rootViewController = initialViewController
+        window?.makeKeyAndVisible()
+    }
 }
 
