fa9271f11c484d935788d572c88ce90ece1dada0
Dustin Hsiang <dustin0116@gmail.com>
2017-06-27 10:38:42 -0700

    subclassed NSObject and implemented NSCoding Protocol in User class.

--- Makestagram/Makestagram/Models/User.swift
+++ Makestagram/Makestagram/Models/User.swift
@@ -9,7 +9,7 @@
 import Foundation
 import FirebaseDatabase.FIRDataSnapshot
 
-class User {
+class User: NSObject {
     
     // MARK: - Properties
     
@@ -21,6 +21,7 @@ class User {
     init(uid: String, username: String) {
         self.uid = uid
         self.username = username
+        super.init()
     }
     
     init?(snapshot: DataSnapshot) {
@@ -30,6 +31,18 @@ class User {
         
         self.uid = snapshot.key
         self.username = username
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        guard let uid = aDecoder.decodeObject(forKey: Constants.UserDefaults.uid) as? String,
+              let username = aDecoder.decodeObject(forKey: Constants.UserDefaults.username) as? String
+            else {
+                return nil
+        }
+        self.uid = uid
+        self.username = username
+        super.init()
     }
     
     private static var _current: User?
@@ -43,5 +56,12 @@ class User {
     
     static func setCurrent (_ user: User) {
         _current = user
+    }
+}
+
+extension User: NSCoding {
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(uid, forKey: Constants.UserDefaults.uid)
+        aCoder.encode(username, forKey: Constants.UserDefaults.username)
     }
 }
