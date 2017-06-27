4c7f6497862fe0287e8ad13f261c24d9813934b4
Dustin Hsiang <dustin0116@gmail.com>
2017-06-27 13:17:50 -0700

    added "Upload from library" and "Cancel" buttons after tapping the "+" tab item.

--- Makestagram/Makestagram/Controllers/MainTabBarController.swift
+++ Makestagram/Makestagram/Controllers/MainTabBarController.swift
@@ -8,25 +8,25 @@
 
 import UIKit
 
-class MainTabBarViewController: UITabBarController {
+class MainTabBarController: UITabBarController {
+    let photoHelper = MGPhotoHelper()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        photoHelper.completionHandler = {
+            image in print("handle image")
+        }
         delegate = self
         tabBar.unselectedItemTintColor = .black
     }
-    
 }
 
-extension MainTabBarViewController: UITabBarControllerDelegate {
+extension MainTabBarController: UITabBarControllerDelegate {
     func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
         if viewController.tabBarItem.tag == 1 {
-            
-            print("take photo")
-            
+            photoHelper.presentActionSheet(from: self)
             return false
-        } else {
-            return true
         }
+        return true
     }
 }
